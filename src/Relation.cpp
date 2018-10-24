662921bc9b8559a6ccd064bbada25da239a9956b
Michael Bzms <35953365+michaelbzms@users.noreply.github.com>
2018-10-24 13:31:55 +0300

    First Phase testing
    Added a DEBUG print method to Relation. Partition seems to be working (at least for the example in the assignment).
    Need to figure out how to pick H1_N: the amount of bits taken into account from H1 hash function, as it also affects the number of buckets (which should not be more than the size of the Relation)

--- src/Relation.cpp
+++ src/Relation.cpp
@@ -1,18 +1,19 @@
 #include <cmath>
+#include <cstdio>
 #include "stdint.h"
 #include "../Headers/Relation.h"
 
 
-unsigned int H1(intField value){
+unsigned int H1(intField value, unsigned int n){
     intField mask = 0;
-    for (int i = 0 ; i < H1_N ; i++ ){
+    for (int i = 0 ; i < n ; i++ ){
         mask |= 0x01<<i;
     }
     return (unsigned int) (mask & value);
 }
 
 
-Relation::Relation(unsigned int _size, intField *_joinField, unsigned int *_rowids) : size(_size), joinField(_joinField), rowids(_rowids), Psum(NULL) {}
+Relation::Relation(unsigned int _size, intField *_joinField, unsigned int *_rowids) : size(_size), joinField(_joinField), rowids(_rowids), Psum(NULL), numberOfBuckets(0) {}
 
 Relation::~Relation() {
     if (Psum != NULL) delete[] Psum;
@@ -22,13 +23,14 @@ Relation::~Relation() {
 
 // phase 1: partition in place Relation R into buckets and fill Psum to distinguish them (|Psum| = 2^n)
 bool Relation::partitionRelation() {
+    const unsigned int H1_N = pickH1_N();
     const unsigned int num_of_buckets = pow(2, H1_N);
     // 1) calculate Hist (in linear time)
     unsigned int *Hist = new unsigned int[num_of_buckets];
     for (unsigned int i = 0 ; i < num_of_buckets ; i++){ Hist[i] = 0; }
     unsigned int *bucket_nums = new unsigned int[size];
     for (unsigned int i = 0 ; i < size ; i++){
-        bucket_nums[i] = H1(joinField[i]);
+        bucket_nums[i] = H1(joinField[i], H1_N);
         if ( bucket_nums[i] < 0 || bucket_nums[i] >= num_of_buckets ){ delete[] Hist; delete[] bucket_nums; return false; }  // ERROR CHECK
         Hist[bucket_nums[i]]++;
     }
@@ -40,6 +42,7 @@ bool Relation::partitionRelation() {
         sum += temp;
     }
     Psum = Hist;
+    numberOfBuckets = num_of_buckets;
     // 3) create new re-ordered versions for joinField and rowids based on their bucket_nums (in linear time)
     intField *newJoinField = new intField[size];
     unsigned int *newRowids = new unsigned int[size];
@@ -48,7 +51,7 @@ bool Relation::partitionRelation() {
     for (unsigned int i = 0 ; i < num_of_buckets ; i++){ nextBucketPos[i] = 0; }
     for (unsigned int i = 0 ; i < size ; i++) {
         const int pos = Psum[bucket_nums[i]] + nextBucketPos[bucket_nums[i]];   // value's position in the re-ordered version
-        if ( pos < 0 || pos >= size ) { delete[] newJoinField; delete[] newRowids; delete[] bucket_nums; delete[] nextBucketPos;  return false; }  // ERROR CHECK
+        if ( pos < 0 || pos >= size ) { delete[] newJoinField; delete[] newRowids; delete[] bucket_nums; delete[] nextBucketPos; Psum = NULL; numberOfBuckets = 0;  return false; }  // ERROR CHECK
         newJoinField[pos] = joinField[i];
         newRowids[pos] = rowids[i];
         nextBucketPos[bucket_nums[i]]++;
@@ -62,3 +65,24 @@ bool Relation::partitionRelation() {
     rowids = newRowids;
     return true;
 }
+
+// pick a good N value for H1 hash function
+unsigned int Relation::pickH1_N() {       //TODO: make a better scheme?
+    return ( (unsigned int) (sqrt(size) / 2 + 1)) % ((unsigned int) sqrt(CPU_CACHE));    // this way number of buckets is about half of the Relation's tuples, modded by the CPU's cache
+}
+
+// DEBUG
+void Relation::printDebugInfo() {
+    if (Psum != NULL) {
+        printf("This Relation is partitioned.\n%u buckets created with Psum as follows:\n", numberOfBuckets);
+        for (unsigned int i = 0; i < numberOfBuckets; i++) {
+            printf("Psum[%u] = %u\n", i, Psum[i]);
+        }
+    }
+    printf("\n joinField | rowids\n");
+    for (unsigned int i = 0 ; i < size ; i++){
+        printf("%10u | %u\n", (unsigned int) joinField[i], rowids[i]);
+    }
+}
+
+
--- test.cpp
+++ test.cpp
@@ -1,7 +1,8 @@
 #include "Headers/RadixHashJoin.h"
 #include <stdio.h>
 
-#define try(a) fprintf(stderr,"%s\n",#a);a();
+
+#define try(a) fprintf(stdout,"%s\n",#a);a();
 #define assert(a) if (!(a)) fprintf(stderr,"assert %s failed\n",#a);
 
 void test_index() {
@@ -10,7 +11,24 @@ void test_index() {
 	assert(3==2);
 }
 
+void test_partition(){
+    intField *joinField = new intField[7];
+    joinField[0] = 'a';
+    joinField[1] = 'b';
+    joinField[2] = 'a';
+    joinField[3] = 'a';
+    joinField[4] = 'c';
+    joinField[5] = 'b';
+    joinField[6] = 'c';
+    unsigned int *rowids = new unsigned int[7];
+    for (unsigned int i = 0 ; i < 7 ; i++) { rowids[i] = i+1; }
+    Relation *R = new Relation(7, joinField, rowids);
+    assert( R->partitionRelation() )
+    R->printDebugInfo();
+    delete R;
+}
+
 int main() {
-	try(test_index);		
+	try(test_partition);
 	return 0;
 }
