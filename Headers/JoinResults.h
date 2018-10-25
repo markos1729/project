4f772c56acc3bb6e6af602ba98b4f176311b0388
Michael Bzms <35953365+michaelbzms@users.noreply.github.com>
2018-10-25 14:01:11 +0300

    joinResults Implementation

--- Headers/JoinResults.h
+++ Headers/JoinResults.h
@@ -7,20 +7,29 @@
 
 using namespace std;
 
+
 #define BUFFER_SIZE (1024*1024)    // buffer size for each ResultNode
 
+
 struct ResultNode{
-    intField buffer[BUFFER_SIZE / sizeof(intField)];
-    ResultNode *next;
-    ResultNode() : next(NULL) {}
+    private:
+        unsigned int buffer[BUFFER_SIZE / sizeof(unsigned int)];
+        int nextpos;
+    public:
+        ResultNode *next;
+        ResultNode();
+        bool isFull();
+        bool addTuple(unsigned int val1, unsigned int val2);
 };
 
 
 class Result {
 private:
     ResultNode *head;
+    ResultNode *cur;
 public:
-    Result() : head(NULL) {}
+    Result();
+    bool addTuple(unsigned int rowid1, unsigned int rowid2);
 };
 
 #endif
--- src/JoinResults.cpp
+++ src/JoinResults.cpp
@@ -1,1 +1,34 @@
 #include "Headers/JoinResults.h"
+
+ResultNode::ResultNode() : next(NULL), nextpos(0) {}
+
+bool ResultNode::isFull() {    // can not fit one more tuple
+    return nextpos > (BUFFER_SIZE / sizeof(unsigned int) - 2);
+}
+
+bool ResultNode::addTuple(unsigned int val1, unsigned int val2) {
+    if (nextpos < 0 || nextpos >= BUFFER_SIZE / sizeof(unsigned int) ) return false;
+    buffer[nextpos] = val1;
+    buffer[nextpos+1] = val2;
+    nextpos += 2;
+    return true;
+}
+
+Result::Result() : head(NULL), cur(NULL) {}
+
+bool Result::addTuple(unsigned int rowid1, unsigned int rowid2) {
+    if (head == NULL){
+        head = new ResultNode;
+        cur = head;
+        return head->addTuple(rowid1, rowid2);
+    } else {
+        if ( ! cur->isFull() ){
+            return cur->addTuple(rowid1, rowid2);
+        } else{
+            ResultNode *temp = cur;
+            cur = new ResultNode;
+            temp->next = cur;
+            return cur->addTuple(rowid1, rowid2);
+        }
+    }
+}
