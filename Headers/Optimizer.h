04995b08a86ab36aa77da737db4ecbac6320c41b
TsiarasKon <tsiaraskon@gmail.com>
2019-01-04 02:01:25 +0200

    BestTree backbone

--- Headers/Optimizer.h
+++ Headers/Optimizer.h
@@ -7,53 +7,80 @@
 using namespace std;
 
 class Optimizer {
-	private:
-		unsigned int nrel;  //number of relations
-		unsigned int *ncol; //number of columns for each relation
-				
-		intField **I;     //minimum value for each column
-		intField **U;     //maximum value for each column
-		unsigned int **F; //number of rows for each column
-		unsigned int **D; //number of distinct values for each column
+private:
+    unsigned int nrel;  //number of relations
+    unsigned int *ncol; //number of columns for each relation
 
-		SQLParser parser;   //parser for this query
-		unsigned int **N;   //bitmap size for each column
-		uint64_t ***bitmap; //compact bitmap for each column
+    intField **I;     //minimum value for each column
+    intField **U;     //maximum value for each column
+    unsigned int **F; //number of rows for each column
+    unsigned int **D; //number of distinct values for each column
 
-		void filter();
+    SQLParser parser;   //parser for this query
+    unsigned int **N;   //bitmap size for each column
+    uint64_t ***bitmap; //compact bitmap for each column
 
-	public:
-		Optimizer(unsigned int _nrel,SQLParser _parser) : nrel(_nrel), parser(_parser) {
-			ncol=new unsigned int[nrel];
-			I=new intField*[nrel];
-			U=new intField*[nrel];
-			F=new unsigned int*[nrel];
-			D=new unsigned int*[nrel];
-			N=new unsigned int*[nrel];
-			bitmap=new uint64_t**[nrel];
-			}
-			
-		~Optimizer() {
-			for (unsigned int r=0; r<nrel; r++) {
-				delete[] I[r];
-				delete[] U[r];
-				delete[] F[r];
-				delete[] D[r];
-				delete[] N[r];
-				delete I;
-				delete U;
-				delete F;
-				delete D;
-				delete N;
-				for (unsigned int c=0; c<ncol[r]; c++) delete[] bitmap[r][c];
-				delete bitmap[r];
-				delete bitmap;
-				}
-			delete[] ncol;
-			}
-		
-		void initialize(unsigned int rid,unsigned int rows,unsigned int cols,intField **columns);
-		void best_plan();
-	};
+    void filter();
+
+    class JoinTree {
+    public:
+        int *rowJoinOrder;
+        int nextRelOrder;
+        intField *treeI;
+        intField *treeU;
+        unsigned int *treeF;
+        unsigned int *treeD;
+        JoinTree(int _nrel, int relId, intField *relI, intField *relU, unsigned int *relF, unsigned int *relD)
+                : treeI(relI), treeU(relU), treeF(relF), treeD(relD) {
+            rowJoinOrder = new int[_nrel]();
+            rowJoinOrder[relId] = 1;
+            nextRelOrder = 2;
+        };
+        JoinTree(JoinTree *currBestTree, int _nrel, int relId, intField *relI, intField *relU, unsigned int *relF, unsigned int *relD) {
+            rowJoinOrder = new int[_nrel];
+            rowJoinOrder[relId] = nextRelOrder;
+            nextRelOrder++;
+            // TODO: calculate stats
+            // TODO: on multiple columns???
+        };
+        ~JoinTree() {
+            delete[] rowJoinOrder;
+        };
+    };
+
+public:
+    Optimizer(unsigned int _nrel,SQLParser _parser) : nrel(_nrel), parser(_parser) {
+        ncol=new unsigned int[nrel];
+        I=new intField*[nrel];
+        U=new intField*[nrel];
+        F=new unsigned int*[nrel];
+        D=new unsigned int*[nrel];
+        N=new unsigned int*[nrel];
+        bitmap=new uint64_t**[nrel];
+    }
+
+    ~Optimizer() {
+        for (unsigned int r=0; r<nrel; r++) {
+            delete[] I[r];
+            delete[] U[r];
+            delete[] F[r];
+            delete[] D[r];
+            delete[] N[r];
+            delete I;
+            delete U;
+            delete F;
+            delete D;
+            delete N;
+            for (unsigned int c=0; c<ncol[r]; c++) delete[] bitmap[r][c];
+            delete bitmap[r];
+            delete bitmap;
+        }
+        delete[] ncol;
+    }
+
+    void initialize(unsigned int rid,unsigned int rows,unsigned int cols,intField **columns);
+    int *best_plan();
+};
+
 
 #endif
--- src/Optimizer.cpp
+++ src/Optimizer.cpp
@@ -1,10 +1,11 @@
 #include <cmath>
 #include <climits>
+#include <unordered_map>
+#include <algorithm>
+#include "../Headers/macros.h"
 #include "../Headers/Optimizer.h"
 
 #define BIG_N 50000000
-#define max(x,y) (x>y ? x : y)
-#define min(x,y) (x<y ? x : y)
 
 void Optimizer::initialize(unsigned int rid,unsigned int rows,unsigned int cols,intField **columns) {
 	ncol[rid]=cols;
@@ -16,20 +17,20 @@ void Optimizer::initialize(unsigned int rid,unsigned int rows,unsigned int cols,
 	bitmap[rid]=new uint64_t*[cols];
 	
 	for (unsigned int c=0; c<cols; c++) {
-		intField u=0;
+        intField u=0;
 		intField i=ULLONG_MAX;
 		
 		for (unsigned int r=0; r<rows; r++) {
 			i=min(i,columns[c][r]);
 			u=max(u,columns[c][r]);
-			}
+		}
 		
 		I[rid][c]=i;
 		U[rid][c]=u;
 		F[rid][c]=rows;
 		D[rid][c]=0;
 	
-		N[rid][c]=min(U[rid][c]-I[rid][c],BIG_N);
+		N[rid][c]=MIN(U[rid][c]-I[rid][c],BIG_N);
 		bitmap[rid][c]=new uint64_t[N[rid][c]/64+1]();
 		
 		for (unsigned int r=0; r<rows; r++) {
@@ -37,10 +38,10 @@ void Optimizer::initialize(unsigned int rid,unsigned int rows,unsigned int cols,
 			if (bitmap[rid][c][cell/64] & (1<<(cell%64))==0) {
 				D[rid][c]++;
 				bitmap[rid][c][cell/64]|=1<<(cell%64);
-				}
 			}
 		}
 	}
+}
 
 void Optimizer::filter() {
 	for (unsigned int f=0; f<parser.nfilters; f++) {
@@ -59,29 +60,29 @@ void Optimizer::filter() {
 			if (bitmap[rel][col][cell/64] & (1<<(cell%64))) {
 				F[rel][col]=F[rel][col]/D[rel][col];
 				D[rel][col]=1;
-				}
-			else D[rel][col]=F[rel][col]=0;
 			}
+			else D[rel][col]=F[rel][col]=0;
+		}
 		
 		if (cmp=='<') {
 			if (value-1>=U[rel][col]) continue;
 			U[rel][col]=value-1;
 			D[rel][col]=D[rel][col]*double(value-1-I[rel][col])/(U[rel][col]-I[rel][col]);
 			F[rel][col]=F[rel][col]*double(value-1-I[rel][col])/(U[rel][col]-I[rel][col]);
-			}
+		}
 			
 		if (cmp=='>') {
 			if (value+1<=I[rel][col]) continue;
 			I[rel][col]=value+1;
 			D[rel][col]=D[rel][col]*double(-value-1+U[rel][col])/(U[rel][col]-I[rel][col]);
 			F[rel][col]=F[rel][col]*double(-value-1+U[rel][col])/(U[rel][col]-I[rel][col]);
-			}
+		}
 		
 		for (unsigned int c=0; c<ncol[rel]; c++) if (c!=col) {
 			D[rel][c]=D[rel][c]*(1-pow((1-float(F[rel][col])/pF),float(F[rel][c])/D[rel][c]));
 			F[rel][c]=F[rel][col];
-			}
 		}
+	}
 		
 	for (unsigned int p=0; p<parser.npredicates; p++) {
 		unsigned int rela=parser.predicates[p].rela_id;
@@ -101,10 +102,63 @@ void Optimizer::filter() {
 		for (unsigned int c=0; c<ncol[rela]; c++) if (c!=cola && c!=colb) {
 			D[rela][c]=D[rela][c]*(1-pow((1-float(F[rela][cola])/pF),float(F[rela][c])/D[rela][c]));
 			F[rela][c]=F[rela][cola];
-			}
 		}
 	}
+}
 
-void Optimizer::best_plan() {
+bool ctob(char boolChar) {
+	return (boolChar == '1');
+}
+
+char btoc(bool charBool) {
+	return (charBool) ? '1' : '0';
+}
+
+string getCombinedIdStr(string str1, string str2) {
+	string newRelId;
+	for (int i = 0; i < str1.length(); i++) {
+		newRelId += btoc(ctob(str1[i]) | ctob(str2[i]));
 	}
+	return newRelId;
+}
 
+int *Optimizer::best_plan() {
+	filter();		// TODO: does this belong elswhere?
+	unordered_map<string, class JoinTree*> BestTree;
+	JoinTree *currTree;
+	string RIdStr[nrel];
+	for (int i = 0; i < nrel; i++) {
+		RIdStr[i].append(nrel, '0');
+		RIdStr[i][i] = '1';
+		// e.g. RIdStr for rel 2 with nrel=4 would be "0010" (relIds start from 0)
+		currTree = new JoinTree(nrel, i, I[i], U[i], F[i], D[i]);
+		BestTree[RIdStr[i]] = currTree;
+	}
+	for (int i = 1; i < nrel; i++) {
+		string SIdStr(nrel - i, '0');
+		SIdStr.append((unsigned int) i, '1');
+		sort(SIdStr.begin(), SIdStr.end());
+		do {	// get all treeIdStr permutations with exactly i '1's
+			for (int j = 0; j < nrel; j++) {
+				// if R[j] is included in S or is not to be joined with S, continue
+				if ( SIdStr[j] == '1' || !connected(parser, j, SIdStr) ) continue;
+				int colJoined;
+				currTree = new JoinTree(BestTree[SIdStr], nrel, i, I[i], U[i], F[i], D[i]);
+				// TODO: get colJoined when creating JoinTree
+				string newSIdStr = getCombinedIdStr(SIdStr, RIdStr[i]);
+				// if there's no BestTree yet for newS or its cost is greater than currTree
+				if ( BestTree.find(newSIdStr) == BestTree.end() || BestTree[SIdStr]->treeF[colJoined] < currTree->treeF[colJoined] ) {
+					BestTree[newSIdStr] = currTree;
+				}
+			}
+		} while ( next_permutation(SIdStr.begin(), SIdStr.end()) );
+	}
+	int *bestJoinOrder = new int[nrel];
+	string bestTreeIdStr(nrel, '1');
+	currTree = BestTree[bestTreeIdStr];
+	for (int i = 0; i < nrel; i++) {
+		bestJoinOrder[i] = currTree->rowJoinOrder[i];
+	}
+	// TODO delete Trees
+	return bestJoinOrder;
+}
